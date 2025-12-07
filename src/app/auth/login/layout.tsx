cca297991e14cf9ea4cd0a9b2f0ee90f917202c4
Didier <didiereste@gmail.com>
2025-12-07 12:28:26 -0500

    [Feature]: Vista completa login (Front-end)

---
+++ src/app/auth/login/layout.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import Image from 'next/image';
+
+type AuthLayoutProps = {
+    children: React.ReactNode;
+}
+export default function AuthLayout({ children }: AuthLayoutProps) {
+    return (
+        <>
+            <div className="min-h-screen bg-gradient-to-b from-blue-100 to-blue-200 flex items-center justify-center ">
+                <div className="w-full max-w-md shadow-2xl px-5 rounded-2xl">
+                    <div className="flex justify-center mb-6 mt-4">
+                        <Image
+                            src="/images/login/copa-de-oro.png"
+                            alt="Login Illustration"
+                            width={120}
+                            height={300}
+                        />
+                    </div>
+                    <div>{children}</div>
+                </div>
+            </div>
+        </>
+    )
+}
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,16 +1,18 @@
 "use client";
 
 import React from "react";
+import { type LucideIcon } from "lucide-react";
+
 
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
-  label: string;
-  className?: string;
+  icon ?: LucideIcon;
+  text : string;
 };
 
-export default function Button({ label,className='', ...props }: ButtonProps) {
+export default function Button({ text,className='',icon: Icon, ...props }: ButtonProps) {
   return (
-    <button {...props} className={`bg-blue-500 text-white px-4 py-2 rounded ${className}`}>
-     {label}
+    <button {...props} className={` flex justify-center items-center ${className} bg-blue-500 text-white px-4 py-2 font-semibold rounded-full hover:bg-blue-600 transition-colors shadow-2xl cursor-pointer space-x-3`}>
+    {Icon && <Icon size={23} />}<span>{text}</span>
     </button>
   );
 }
