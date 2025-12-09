70b5af544e39f561577dfef9784e0ecb3ac2348c
Elvismontoya <elvisam08@gmail.com>
2025-12-09 09:54:38 -0500

    Visual de crear torneo

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -7,7 +7,7 @@ interface AuthLayoutProps {
 
 export default function AuthLayout({ children }: AuthLayoutProps) {
     return (
-        <div className="min-h-screen bg-gradient-to-b from-blue-200 to-blue-200 flex justify-start items-start">
+        <div className="min-h-screen bg-gradient-to-b from-blue-200 to-blue-200 flex justify-start items-start ">
             <div className="w-full max-w-sm p-10 items-center justify-center my-auto">
                 <div className="flex justify-center mb-16">
                     <div className="w-32">
@@ -20,21 +20,20 @@ export default function AuthLayout({ children }: AuthLayoutProps) {
                     </div>
                 </div>
 
-                {/* Botones con nueva paleta */}
-                <div className="w-full bg-blue-100 rounded-3xl p-8 space-y-8 flex flex-col">
-                    <button className="w-full bg-[var(--color-button-background)] text-white font-bold py-4 rounded-2xl text-lg hover:bg-blue-600">
+                <div className="w-full bg-blue-100 rounded-3xl p-8 space-y-8 flex flex-col shadow-xl">
+                    <button className="w-full bg-[var(--color-button-background)] text-white font-bold py-4 rounded-2xl text-lg hover:bg-blue-600 shadow-xl">
                         Dashboard
                     </button>
 
-                    <button className="w-full bg-[var(--color-button-background)] text-white font-bold py-4 rounded-2xl text-lg hover:bg-blue-600">
+                    <button className="w-full bg-[var(--color-button-background)] text-white font-bold py-4 rounded-2xl text-lg hover:bg-blue-600 shadow-xl">
                         Torneos
                     </button>
 
-                    <button className="w-full bg-[var(--color-button-background)] text-white font-bold py-4 rounded-2xl text-lg hover:bg-blue-600">
+                    <button className="w-full bg-[var(--color-button-background)] text-white font-bold py-4 rounded-2xl text-lg hover:bg-blue-600 shadow-xl">
                         Análisis
                     </button>
 
-                    <button className="w-full bg-[var(--color-button-background)] text-white font-bold py-4 rounded-2xl text-lg hover:bg-blue-600">
+                    <button className="w-full bg-[var(--color-button-background)] text-white font-bold py-4 rounded-2xl text-lg hover:bg-blue-600 shadow-xl">
                         Configuración
                     </button>
                 </div>
@@ -44,8 +43,7 @@ export default function AuthLayout({ children }: AuthLayoutProps) {
                 </p>
             </div>
 
-            {/* Columna derecha: contenido dinámico */}
-            <div className="flex-1 p-10 text-black">
+            <div className="flex-1 p-8 text-black">
                 {children}
             </div>
         </div>
